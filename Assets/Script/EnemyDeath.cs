98f55725f40790f5e2ce96a721d832ece07a22c6
Tanguy Chanal <tanguy.chanal@gmail.com>
2017-01-26 17:09:31 +0100

    Enemy death
    Implemented the script allowing to destroy an enemy, along with a death
    animation for the Octorok enemy.

---
+++ Assets/Script/EnemyDeath.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDeath : MonoBehaviour {
+
+	public Animator anim;
+	public AnimationClip animDeath;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void kill(){
+		anim.SetBool("dying", true);
+		Destroy (this.gameObject, animDeath.length);
+	}
+
+}
