9f78acf0087d0e1f48fd38867cc0b893513f6bd5
Tanguy Chanal <tanguy.chanal@gmail.com>
2017-01-30 17:35:13 +0100

    Enemy controller
    Added a script that handles the enemies' life points.

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public EnemyDeath enemy;
+
+	public int lifePoints = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.lifePoints <= 0){
+			SufferDeath ();
+		}
+	}
+
+	void SufferDamage(int hit){
+		this.lifePoints = this.lifePoints - hit;
+	}
+
+	void SufferDeath(){
+		enemy.kill ();
+	}
+
+}
