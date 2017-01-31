9894b3773ffbc254ae8fba2faea2681efd6cc93c
Tanguy Chanal <tanguy.chanal@gmail.com>
2017-01-31 15:23:31 +0100

    Enemy health handling
    Changed the way enemy health is handled by fusing EnemyDeath and
    EnemyController into EnemyHealth.

---
+++ Assets/Script/Enemies/EnemyHealth.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public EnemyMovement em;
+	public Animator anim;
+	public AnimationClip animDeath;
+
+	public GameObject enemySprite;
+
+	private bool isHurt = false;
+	public int lifePoints = 3;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(this.lifePoints <= 0){
+			SufferDeath ();
+		}
+	}
+
+	public void SufferKnockback(Vector2 knock){
+
+	}
+
+	public void SufferDamage(int hit){
+		this.lifePoints = this.lifePoints - hit;
+		isHurt = true;
+		StartCoroutine(Blink ());
+	}
+
+	void SufferDeath(){
+		em.setVelocity (new Vector2(0,0));
+		Destroy (this.gameObject.GetComponent<CircleCollider2D> ());
+		anim.SetBool("dying", true);
+		Destroy (this.gameObject, animDeath.length);
+	}
+
+	IEnumerator Blink(){
+		SpriteRenderer es = enemySprite.GetComponent<SpriteRenderer> ();
+		for (int n = 0; n < 3; n++) {
+			es.color = Color.red;
+			yield return new WaitForSeconds (0.1f);
+			es.color = Color.white;
+			yield return new WaitForSeconds (0.1f);
+		}
+	}
+}
