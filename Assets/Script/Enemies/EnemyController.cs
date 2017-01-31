0c3d900ff7316b21cdf9bc12e66932407df01729
Tanguy Chanal <tanguy.chanal@gmail.com>
2017-01-31 14:34:00 +0100

    Blinking
    Enemy blinks when hurt.

--- Assets/Script/Enemies/EnemyController.cs
+++ Assets/Script/Enemies/EnemyController.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class EnemyController : MonoBehaviour {
 
 	public EnemyDeath enemy;
+	public GameObject enemySprite;
 
+	private bool isHurt = false;
 	public int lifePoints = 3;
 
 	// Use this for initialization
@@ -25,10 +27,22 @@ public class EnemyController : MonoBehaviour {
 
 	public void SufferDamage(int hit){
 		this.lifePoints = this.lifePoints - hit;
+		isHurt = true;
+		StartCoroutine(Blink ());
 	}
 
 	void SufferDeath(){
 		enemy.kill ();
+	}
+
+	IEnumerator Blink(){
+		SpriteRenderer es = enemySprite.GetComponent<SpriteRenderer> ();
+		for (int n = 0; n < 3; n++) {
+			es.color = Color.red;
+			yield return new WaitForSeconds (0.1f);
+			es.color = Color.white;
+			yield return new WaitForSeconds (0.1f);
+		}
 	}
 
 }
