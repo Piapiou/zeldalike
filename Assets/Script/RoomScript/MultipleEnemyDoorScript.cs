51b686c53990439f4b504709561d4e2f034c8dd7
Pierre Sénéclauze <Pierre Snclauze>
2017-01-31 17:31:18 +0100

    Donjon de test terminé !

---
+++ Assets/Script/RoomScript/MultipleEnemyDoorScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultipleEnemyDoorScript : MonoBehaviour {
+
+    public GameObject[] octorok;
+    public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (door.activeSelf)
+        {
+            bool octo = false;
+            for (int i = 0; i < octorok.Length; i++)
+            {
+                octo |= (octorok[i] != null);
+            }
+
+            door.SetActive(octo);
+        }
+	}
+}
