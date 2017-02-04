a7c5a10e750c9367db07b9adfb78136ea69a633d
Tanguy Chanal <tanguy.chanal@gmail.com>
2017-02-04 19:01:28 +0100

    Enemy drop
    Created a script allowing enemies to drop items upon dying.

---
+++ Assets/Script/Enemies/EnemyDrop.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDrop : MonoBehaviour {
+
+	public GameObject item;
+	public float dropRate = 1.0f;
+	public Transform position;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDestroy(){
+		dropItem ();
+	}
+
+	void dropItem(){
+		float rand = Random.value;
+		if (item != null) {
+			if (rand <= dropRate) {
+				GameObject droppedItem = GameObject.Instantiate (item);
+				droppedItem.transform.position = setItemPosition ();
+			}
+		}
+	}
+
+	Vector3 setItemPosition(){
+		Vector3 itemPosition = position.position;
+		itemPosition += new Vector3 (0, -0.2f, 0);
+		return itemPosition;
+	}
+
+}
