5956947d9dc7971d7721b773225cec37b04fdb58
Tanguy Chanal <tanguy.chanal@gmail.com>
2017-02-04 15:50:00 +0100

    Music test
    Fidgeting with music implementation.

---
+++ Assets/Script/MusicController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+
+	public GameObject camera;
+	public AudioClip newMusic;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnColliderEnter2D(Collision2D coll)
+	{
+		AudioSource source = camera.gameObject.GetComponent<AudioSource> ();
+		source.clip = newMusic;
+		source.Play ();
+	}
+
+}
